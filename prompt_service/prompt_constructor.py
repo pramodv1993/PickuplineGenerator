385508f9a636e22c65e36e87db4eb68c4bcc4131
pramodv1993 <pramodv1993@gmail.com>
2023-02-26 13:46:09 +0100

    clean

--- prompt_service/prompt_constructor.py
+++ prompt_service/prompt_constructor.py
@@ -4,8 +4,8 @@ class PromptConstructor:
     def __init__(self, prof1: dict, prof2: dict):
         self.prof1 = prof1
         self.prof2 = prof2
-        self.prompt = '''P1, P2 are two people.
-        '''
+        self.prompt = "P1, P2 are two people."
+        
     def _add_profiles(self):
         self.prompt += ('\nP1 has the profile:\n') +\
                         ('\n'.join([f'{str(k)}:{str(v)}' for k,v in self.prof1.items()]))
@@ -13,12 +13,15 @@ class PromptConstructor:
                          ('\n'.join([f'{str(k)}:{str(v)}' for k,v in self.prof2.items()]))
 
     def _add_history(self, history: List[str]):
-        '''history: ['P1:msg1', P2:'msg2'..]'''
+        '''
+        history: ['P1:msg1', P2:'msg2'..]
+        '''
         self.prompt += ("\n\nP1 and P2's conversations so far:\n") +\
                          ('\n'.join(history))
     
     def _add_reply_requirement(self, reply_to: str, reply_attr: List[str]):
-        '''reply_attr: ['witty', 'funny'...]
+        '''
+        reply_attr: ['witty', 'funny'...]
         reply_to: P1/P2
         '''
         _from = 'P1' if reply_to=='P2' else 'P2'
@@ -30,8 +33,7 @@ class PromptConstructor:
         self.history = history
         self.reply_attr = reply_attr
         self.reply_to = reply_to
-        return self
-
+    
     def construct(self):
         self._add_profiles()
         self.reply_attr = ['witty'] if not len(self.reply_attr) else self.reply_attr
